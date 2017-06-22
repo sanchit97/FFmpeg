883797a296234f03c746dbb2e1e6465cfeaaf926
Sanchit Sinha <sanchit15083@iiitd.ac.in>
2017-06-23 04:53:09 +0530

    Coding Style Improve
    Signed-off-by: Sanchit Sinha <sanchit15083@iiitd.ac.in>

--- libavfilter/af_ambisonic.c
+++ libavfilter/af_ambisonic.c
@@ -28,8 +28,7 @@
 #include <stdio.h>
 
 enum FilterType {
-    shelf,
-    phaseshift
+    shelf
 };
 
 typedef struct Cache {
@@ -49,78 +48,76 @@ enum Layouts2D {
 };
 
 enum Layouts3D {
-    CUBE=8,
-    DODECAHEDRON=20,
-
+    CUBE        =8,
+    DODECAHEDRON=20
 };
 
-static const struct
-{
+static const struct {
     float matrix[22][9];
-} ambisonic_matrix2d[]={
-        [MONO]={
-            .matrix={
-                {0.22156, 0, 0, 0},
-            },
+} ambisonic_matrix2d[]= {
+    [MONO]={
+        .matrix={
+            {0.22156, 0, 0, 0},
         },
-        [TRIANGLE]={
-            .matrix={
-                {0.17836, 0.32555, 0.18795},
-                {0.17836, 0      ,-0.37591},
-                {0.17836,-0.32555, 0.18795},
-            },
+    },
+    [TRIANGLE]={
+        .matrix={
+            {0.17836, 0.32555, 0.18795},
+            {0.17836, 0      ,-0.37591},
+            {0.17836,-0.32555, 0.18795},
         },
-        [SQUARE]={
-            .matrix={
-                {0.39388, 0.18690, 0.18690, 0},
-                {0.39388,-0.18690, 0.18690, 0},
-                {0.39388,-0.18690,-0.18690, 0},
-                {0.39388, 0.18690,-0.18690, 0},
-            },
+    },
+    [SQUARE]={
+        .matrix={
+            {0.39388, 0.18690, 0.18690, 0},
+            {0.39388,-0.18690, 0.18690, 0},
+            {0.39388,-0.18690,-0.18690, 0},
+            {0.39388, 0.18690,-0.18690, 0},
         },
-        [PENTAGON]={
-            .matrix={
-                {0.20195, 0      , 0.33420, 0},
-                {0.11356, 0.2901 , 0.04186, 0},
-                {0.19654,-0.07993,-0.34782, 0},
-                {0.19654, 0.07993,-0.34782, 0},
-                {0.19654,-0.2901 , 0.04186, 0},
-            },
+    },
+    [PENTAGON]={
+        .matrix={
+            {0.20195, 0      , 0.33420, 0},
+            {0.11356, 0.2901 , 0.04186, 0},
+            {0.19654,-0.07993,-0.34782, 0},
+            {0.19654, 0.07993,-0.34782, 0},
+            {0.19654,-0.2901 , 0.04186, 0},
         },
-        [HEXAGON]={
-            .matrix={
-                {0.26259, 0      ,  0.31326, 0},
-                {0.26259, 0.27129,  0.15663, 0},
-                {0.26259, 0.27129, -0.15663, 0},
-                {0.26259, 0      , -0.31326, 0},
-                {0.26259,-0.27129, -0.15663, 0},
-                {0.26259,-0.27129,  0.15663, 0},
-            },
+    },
+    [HEXAGON]={
+        .matrix={
+            {0.26259, 0      ,  0.31326, 0},
+            {0.26259, 0.27129,  0.15663, 0},
+            {0.26259, 0.27129, -0.15663, 0},
+            {0.26259, 0      , -0.31326, 0},
+            {0.26259,-0.27129, -0.15663, 0},
+            {0.26259,-0.27129,  0.15663, 0},
         },
-        [HEPTAGON]={
-            .matrix={
-                {0.22501,-0.0    ,  0.26846, 0},
-                {0.22507, 0.20989,  0.16741, 0},
-                {0.22507, 0.26180, -0.05969, 0},
-                {0.22511, 0.11651, -0.24195, 0},
-                {0.22511,-0.11651, -0.24195, 0},
-                {0.22507,-0.26180, -0.05969, 0},
-                {0.22507,-0.20989,  0.16741, 0},
-            },
+    },
+    [HEPTAGON]={
+        .matrix={
+            {0.22501,-0.0    ,  0.26846, 0},
+            {0.22507, 0.20989,  0.16741, 0},
+            {0.22507, 0.26180, -0.05969, 0},
+            {0.22511, 0.11651, -0.24195, 0},
+            {0.22511,-0.11651, -0.24195, 0},
+            {0.22507,-0.26180, -0.05969, 0},
+            {0.22507,-0.20989,  0.16741, 0},
         },
-        [OCTAGON]={
-            .matrix={
-                {0.19694,  0.08991,  0.21706, 0},
-                {0.19694,  0.21706,  0.08991, 0},
-                {0.19694,  0.21706, -0.08991, 0},
-                {0.19694,  0.08991, -0.21706, 0},
-                {0.19694, -0.08991, -0.21706, 0},
-                {0.19694, -0.21706, -0.08991, 0},
-                {0.19694, -0.21706,  0.08991, 0},
-                {0.19694, -0.08991,  0.21706, 0},
-            },
+    },
+    [OCTAGON]={
+        .matrix={
+            {0.19694,  0.08991,  0.21706, 0},
+            {0.19694,  0.21706,  0.08991, 0},
+            {0.19694,  0.21706, -0.08991, 0},
+            {0.19694,  0.08991, -0.21706, 0},
+            {0.19694, -0.08991, -0.21706, 0},
+            {0.19694, -0.21706, -0.08991, 0},
+            {0.19694, -0.21706,  0.08991, 0},
+            {0.19694, -0.08991,  0.21706, 0},
         },
-    };
+    },
+};
 
 typedef struct AmbisonicContext {
     const AVClass *class;
@@ -162,50 +159,32 @@ static const AVOption ambisonic_options[] = {
 
 static int query_formats(AVFilterContext *ctx)
 {
-    AmbisonicContext *s     = ctx->priv;
+    AmbisonicContext *s = ctx->priv;
     AVFilterFormats *formats = NULL;
     AVFilterChannelLayouts *layouts = NULL;
     int temp;
     int ret;
-    int i,j;
 
     memset(s->decode_matrix,0,22*9);
 
     //will be changed
     s->order=1;//first order ambisonics
-    switch(s->dimension)
-    {
-        case 2:
-        s->nb_channels=2*s->order+1;
-        break;
-        case 3:
-        s->nb_channels=(s->order+1)*(s->order+1);
-        break;
-        default:
-        s->nb_channels=2*s->order+1;
+    switch(s->dimension) {
+        case 2:  s->nb_channels=2*s->order+1;                break;
+        case 3:  s->nb_channels=(s->order+1)*(s->order+1);   break;
+        default: s->nb_channels=2*s->order+1;
     }
 
-    switch(s->dimension)
-    {
-        case 2:
-        switch(s->nb_sp)
-        {
-            case 4:  temp=AV_CH_LAYOUT_4POINT0;
-            break;
-            case 5:  temp=AV_CH_LAYOUT_5POINT0;
-            break;
-            case 6:  temp=AV_CH_LAYOUT_6POINT0;
-            break;
-            case 7:  temp=AV_CH_LAYOUT_7POINT0;
-            break;
-            case 8:  temp=AV_CH_LAYOUT_OCTAGONAL;
-            break;
-            // case 16: temp=AV_CH_LAYOUT_HEXADECAGONAL;
-            // break;
+    switch(s->dimension) {
+        case 2: switch(s->nb_sp) {
+            case 4:  temp=AV_CH_LAYOUT_4POINT0;       break;
+            case 5:  temp=AV_CH_LAYOUT_5POINT0;       break;
+            case 6:  temp=AV_CH_LAYOUT_6POINT0;       break;
+            case 7:  temp=AV_CH_LAYOUT_7POINT0;       break;
+            case 8:  temp=AV_CH_LAYOUT_OCTAGONAL;     break;
+            case 16: temp=AV_CH_LAYOUT_HEXADECAGONAL; break;
             default: temp=AV_CH_LAYOUT_4POINT0;
-        }
-        break;
-
+        } break;
         case 3:
         break;
     }
@@ -216,7 +195,6 @@ static int query_formats(AVFilterContext *ctx)
     ret = ff_set_common_formats(ctx, formats);
     if (ret)
         return ret;
-
 
     ret = ff_add_channel_layout(&layouts, temp);
     if (ret)
@@ -235,7 +213,6 @@ static int query_formats(AVFilterContext *ctx)
     ret = ff_channel_layouts_ref(layouts, &ctx->inputs[0]->out_channel_layouts);
     if (ret)
         return ret;
-
 
     formats = ff_all_samplerates();
     if(!formats)
@@ -262,13 +239,10 @@ static void shelf_flt(      AmbisonicContext *s,
     a2 = -a2;
 
     switch(channelno){
-        case 1: s->gain= 1.75;
-                break;
-        case 2: s->gain= -1.26;
-                break;
-        case 3: s->gain= -1.26;
-                break;
-        default: s->gain= 1;
+        case 1:  s->gain=  1.75;  break;
+        case 2:  s->gain= -1.26;  break;
+        case 3:  s->gain= -1.26;  break;
+        default: s->gain=  1;
     }
 
     for (i = 0; i+1 < len; i++) {
@@ -352,89 +326,14 @@ static int config_output(AVFilterLink *outlink)
     return 0;
 }
 
-static float multiply(float decode_matrix[22][9],int row, float *vars[22], int sample_no, int nb_channels)
+static float multiply(const float decode_matrix[22][9],int row, float *vars[22], int sample_no, int nb_channels)
 {
-    int j;
     float sum=0;
-
-    for(j=0;j<nb_channels;j++)
-    {
+    int j;
+    for(j=0;j<nb_channels;j++) {
         sum+=(decode_matrix[row][j]*vars[j][sample_no]);
     }
     return sum;
-}
-
-static void configure_matrix(AmbisonicContext *s)
-{
-    int channels=s->nb_channels;
-    int speakers=s->nb_sp;
-    int dimension=s->dimension;
-
-    // switch(s->dimension)
-    // {
-    //     case 2:
-    //     switch(s->nb_sp)
-    //     {
-    //         case 1:
-    //             s->decode_matrix[0][0]=0.22156;
-    //             s->decode_matrix[1][0]=0.22156;
-    //             s->decode_matrix[2][0]=0.22156;
-    //             s->decode_matrix[3][0]=0.22156;
-    //         break;
-    //         case 3:
-    //             s->decode_matrix[0][0]=0.17836;s->decode_matrix[0][1]=0.32555;s->decode_matrix[0][2]=0.18795;
-    //             s->decode_matrix[1][0]=0.17836;s->decode_matrix[1][1]=0.0;s->decode_matrix[1][2]=-0.37591;
-    //             s->decode_matrix[2][0]=0.17836;s->decode_matrix[2][1]=-0.32555;s->decode_matrix[2][2]=0.18795;
-    //         break;
-    //         case 4:
-    //             s->decode_matrix[0][0]=0.39388;s->decode_matrix[0][1]=0.18690;s->decode_matrix[0][2]=0.18690;
-    //             s->decode_matrix[1][0]=0.39388;s->decode_matrix[1][1]=(-1)*0.18690;s->decode_matrix[1][2]=0.18690;
-    //             s->decode_matrix[2][0]=0.39388;s->decode_matrix[2][1]=(-1)*0.18690;s->decode_matrix[2][2]=(-1)*0.18690;
-    //             s->decode_matrix[3][0]=0.39388;s->decode_matrix[3][1]=0.18690;s->decode_matrix[3][2]=(-1)*0.18690;
-    //         break;
-    //         case 5:
-    //             s->decode_matrix[0][0]=0.20195;s->decode_matrix[0][1]=0;s->decode_matrix[0][2]=        0.33420;
-    //             s->decode_matrix[1][0]=0.11356;s->decode_matrix[1][1]=0.2901;s->decode_matrix[1][2]=   0.04186;
-    //             s->decode_matrix[2][0]=0.19654;s->decode_matrix[2][1]=-0.07993;s->decode_matrix[2][2]=-0.34782;
-    //             s->decode_matrix[3][0]=0.19654;s->decode_matrix[3][1]=0.07993;s->decode_matrix[3][2]= -0.34782;
-    //             s->decode_matrix[4][0]=0.19654;s->decode_matrix[4][1]=-0.2901;s->decode_matrix[4][2]=  0.04186;
-    //         break;
-    //         case 6:
-    //             s->decode_matrix[0][0]=0.26259;s->decode_matrix[0][1]=0       ;s->decode_matrix[0][2]= 0.31326;
-    //             s->decode_matrix[1][0]=0.26259;s->decode_matrix[1][1]=0.27129 ;s->decode_matrix[1][2]= 0.15663;
-    //             s->decode_matrix[2][0]=0.26259;s->decode_matrix[2][1]=0.27129 ;s->decode_matrix[2][2]=-0.15663;
-    //             s->decode_matrix[3][0]=0.26259;s->decode_matrix[3][1]=0       ;s->decode_matrix[3][2]=-0.31326;
-    //             s->decode_matrix[4][0]=0.26259;s->decode_matrix[4][1]=-0.27129;s->decode_matrix[4][2]=-0.15663;
-    //             s->decode_matrix[5][0]=0.26259;s->decode_matrix[5][1]=-0.27129;s->decode_matrix[5][2]= 0.15663;
-    //         break;
-    //         case 7:
-    //             s->decode_matrix[0][0]=0.22501;s->decode_matrix[0][1]= -0.0    ;s->decode_matrix[0][2]= 0.26846;
-    //             s->decode_matrix[1][0]=0.22507;s->decode_matrix[1][1]= 0.20989 ;s->decode_matrix[1][2]= 0.16741;
-    //             s->decode_matrix[2][0]=0.22507;s->decode_matrix[2][1]= 0.26180 ;s->decode_matrix[2][2]=-0.05969;
-    //             s->decode_matrix[3][0]=0.22511;s->decode_matrix[3][1]= 0.11651 ;s->decode_matrix[3][2]=-0.24195;
-    //             s->decode_matrix[4][0]=0.22511;s->decode_matrix[4][1]= -0.11651;s->decode_matrix[4][2]=-0.24195;
-    //             s->decode_matrix[5][0]=0.22507;s->decode_matrix[5][1]= -0.26180;s->decode_matrix[5][2]=-0.05969;
-    //             s->decode_matrix[6][0]=0.22507;s->decode_matrix[6][1]= -0.20989;s->decode_matrix[6][2]= 0.16741;
-    //         break;
-    //         case 8:
-    //             s->decode_matrix[0][0]=0.19694;s->decode_matrix[0][1]= 0.08991;s->decode_matrix[0][2]= 0.21706;
-    //             s->decode_matrix[1][0]=0.19694;s->decode_matrix[1][1]= 0.21706;s->decode_matrix[1][2]= 0.08991;
-    //             s->decode_matrix[2][0]=0.19694;s->decode_matrix[2][1]= 0.21706;s->decode_matrix[2][2]=-0.08991;
-    //             s->decode_matrix[3][0]=0.19694;s->decode_matrix[3][1]= 0.08991;s->decode_matrix[3][2]=-0.21706;
-    //             s->decode_matrix[4][0]=0.19694;s->decode_matrix[4][1]=-0.08991;s->decode_matrix[4][2]=-0.21706;
-    //             s->decode_matrix[5][0]=0.19694;s->decode_matrix[5][1]=-0.21706;s->decode_matrix[5][2]=-0.08991;
-    //             s->decode_matrix[6][0]=0.19694;s->decode_matrix[6][1]=-0.21706;s->decode_matrix[6][2]= 0.08991;
-    //             s->decode_matrix[7][0]=0.19694;s->decode_matrix[7][1]=-0.08991;s->decode_matrix[7][2]= 0.21706;
-    //         break;
-    //     }
-    //     break;
-    //     case 3:
-    //     switch(s->nb_sp)
-    //     {
-    //         // case
-    //     }
-    //     break;
-    // }
 }
 
 static int filter_frame(AVFilterLink *inlink, AVFrame *in)
@@ -458,8 +357,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 
     s->filter = shelf_flt;
 
-    if(s->enable_shelf)
-    {
+    if(s->enable_shelf) {
         // shelf filter
         // for w channel gain= 1.75
         s->filter(s, in->extended_data[0],
@@ -473,7 +371,6 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
                       &s->cache[1].i1, &s->cache[1].i2,
                       &s->cache[1].o1, &s->cache[1].o2,
                       s->b0, s->b1, s->b2, s->a1, s->a2, -1.,1.,2);
-
         s->filter(s, in->extended_data[2],
                       out_buf->extended_data[2], in->nb_samples,
                       &s->cache[2].i1, &s->cache[2].i2,
@@ -481,23 +378,16 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
                       s->b0, s->b1, s->b2, s->a1, s->a2, -1.,1.,3);
     }
 
-
-    // configure_matrix(s);
-
-    for(i=0;i<s->nb_channels;i++)
-    {
+    for(i=0;i<s->nb_channels;i++) {
         vars[i]=(float*)in->extended_data[i];
     }
 
-    for(i=0;i<s->nb_sp;i++)
-    {
+    for(i=0;i<s->nb_sp;i++) {
         c[i]=(float *)out_buf->extended_data[i];
     }
 
-    for(itr=0;itr<in->nb_samples;itr++)
-    {
-        for(i=0;i<s->nb_sp;i++)
-        {
+    for(itr=0;itr<in->nb_samples;itr++) {
+        for(i=0;i<s->nb_sp;i++) {
             if(s->dimension==2){
                 calc[i]=multiply(ambisonic_matrix2d[s->nb_sp].matrix,i,vars,itr,s->nb_channels);
             } else {
@@ -505,8 +395,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
             }
         }
 
-        for(i=0;i<s->nb_sp;i++)
-        {
+        for(i=0;i<s->nb_sp;i++) {
             c[i][itr]=calc[i];
         }
     }
@@ -526,8 +415,8 @@ static const AVFilterPad inputs[] = {
 };
 static const AVFilterPad outputs[] = {
     {
-        .name = "default",
-        .type = AVMEDIA_TYPE_AUDIO,
+        .name         = "default",
+        .type         = AVMEDIA_TYPE_AUDIO,
         .config_props = config_output,
     },
     { NULL }
